0efca921f3ca2f7b749b6e0b045c12a2079de878
NarciSource <jwch11@gmail.com>
2024-04-26 22:34:53 +0900

    fix: ajax, not-found로 오류 처리

--- app/users/[name]/page.jsx
+++ app/users/[name]/page.jsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import Profile from "./_components/Profile";
 import Repositories from "./_components/Repositories";
 import getProfile from "@/network/getProfile";
@@ -6,13 +7,17 @@ import getRepos from "@/network/getRepos";
 export default async function User({ params }) {
     const { name } = params;
 
-    const user = await getProfile(name);
-    const repos = await getRepos(name);
+    try {
+        const user = await getProfile(name);
+        const repos = await getRepos(name);
 
-    return (
-        <>
-            <Profile user={user} />
-            <Repositories repos={repos} />
-        </>
-    );
+        return (
+            <>
+                <Profile user={user} />
+                <Repositories repos={repos} />
+            </>
+        );
+    } catch (error) {
+        notFound();
+    }
 }
